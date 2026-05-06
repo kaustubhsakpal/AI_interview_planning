33c1c7ef23b004121799ad46549ea74b11b30878
kaustubh <kaustubhsakpal9@gmil.com>
2026-05-06 12:37:50 +0530

    port work

--- Backend/server.js
+++ Backend/server.js
@@ -2,9 +2,10 @@ import 'dotenv/config'
 import app from './src/app.js'
 import connectToDB from "./src/config/database.js"
 
+const port = process.env.PORT || 5000;
 connectToDB()
 
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
 })
