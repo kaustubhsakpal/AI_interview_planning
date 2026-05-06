ddba2b8c867a15fc581e338fc0b8cb5806bcbd2d
kaustubh <kaustubhsakpal9@gmil.com>
2026-05-06 13:10:41 +0530

    cros problrm solve

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://ai-interview-planning.vercel.app/",
     credentials: true
 }))
 
