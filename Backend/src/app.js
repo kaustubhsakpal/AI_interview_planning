63bbac639a0603e65429414971f55209e2378978
kaustubh <kaustubhsakpal9@gmil.com>
2026-05-07 08:36:03 +0530

    cros origin

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import cookieParser from 'cookie-parser' 
 import cors from 'cors'
-
+import morgan from 'morgan'
 const app = express()
-
+app.use(morgan("dev"))
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: process.env.BACKEND_URL,
+    baseURL: process.env.VITE_BACKEND_URL,
     withCredentials: true
 })
 
